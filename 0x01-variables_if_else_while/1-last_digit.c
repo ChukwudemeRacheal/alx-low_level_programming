65569741ef5e25633fc4e36cebf488e82d2b26b5
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-19 04:35:38 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,34 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <ctype.h>
 /**
  * main - entry point
  *
- * Description: this code prints the last digits
- * of a random number
+ * Description: prints an alphabet in lower case
  *
  * Return: always 0 (success)
  */
 int main(void)
 {
-	int n;
-	int l_digit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	l_digit = n % 10;
-	printf("Last digit of %d is %d ", n, l_digit);
-	if (l_digit > 5)
+	char abc;
+
+	for (abc = 'a'; abc <= 'z'; ++abc)
 	{
-		printf("and is greater than 5\n");
+		putchar(abc);
 	}
-	else if (l_digit == 0)
-	{
-		printf("and is 0\n");
-	}
-	else
-	{
-		printf("and is less than 6 and not 0\n");
-	}
+	putchar('\n');
 	return (0);
 }
