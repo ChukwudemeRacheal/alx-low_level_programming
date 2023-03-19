f5545dbc9cbd7d1e2be27b89d9fa4c29142c4bb9
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-18 21:16:26 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <sdtlid.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints an alphabet in lower case, followed by a new line.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+
+	char abc;
+
+	for (abc = 'a'; abc <= 'Z'; ++abc)
+	{
+		putchar(abc);
+	}
+	putchar('\n');
+	return (0);
+}
