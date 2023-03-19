96dff76a0e220179c372e168822a1c58af95ec5b
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-19 06:35:53 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: printing digits using putchar
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
