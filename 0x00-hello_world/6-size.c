24f09b8d2ad7ac71907b9b329f1a2baca478f38f
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-19 17:04:59 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,34 @@
+/* this program prints all sizes of data */
 #include <stdio.h>
 /**
-* main - A program that prints the size of various types of computer
-* Return: 0 (Success)
-*/
+ * main - entry point
+ *
+ * Description: This is the point of execution
+ * int - gives the value of the integer
+ * float - give the value of the float
+ * char - give the value of the character
+ * long int - give the value of the long int
+ * long long - gives the value of the long long
+ *
+ * Return: Always gives a zero value to show success
+ */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of int: %lu  byte(s)\n", (unsigned  long)sizeof(b));
-prinf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	char charType;
+	int integerType;
+	long int longintType;
+	long long longlongType;
+	float floatType;
+
+	/**
+	 * sizeof - this function is used to evaluate
+	 * the size of a variable
+	 */
+	printf("Size of a char: %d byte(s)\n", sizeof(charType));
+	printf("Size of an int: %d byte(s)\n", sizeof(integerType));
+	printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %d byte(s)\n", sizeof(floatType));
+
 return (0);
 }
