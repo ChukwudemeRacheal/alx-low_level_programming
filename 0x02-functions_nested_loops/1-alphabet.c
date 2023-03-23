22de1adbd3a74441b463581b7c53290ab68e3f5a
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-23 08:43:39 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - entry point
+*
+* Description: this function prints the alphabets
+* Return: Always 0 success
+*/
+void print_alphabet(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
