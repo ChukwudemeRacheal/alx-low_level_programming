b8b55b89de77b5293bb2e3e04103c404ce3beb57
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-24 05:37:55 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: always 0 success
+*/
+int main(void)
+{
+int i;
+long int fib[50];
+
+fib[0] = 1;
+fib[1] = 2;
+for (i = 2; i < 50; i++)
+{
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+for (i = 0; i < 49; i++)
+{
+printf("%ld, ", fib[i]);
+}
+printf("%ld\n", fib[49]);
+return (0);
+}
