4fa7f8f5ed01c5a71731a5e188329a44dbcb62f2
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-23 21:42:44 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 include main.h
 
 /**
- * main - test function for positve or negative
- * Return: 0
- */
-
+* main - test function for positve or negative
+* Return: 0
+*/
 int main(void)
 {
-	int i;
-	i = 0;
-	positive_or_negative;
-	return (0);
+int i;
+i = 0;
+positive_or_negative;
+return (0);
 }
