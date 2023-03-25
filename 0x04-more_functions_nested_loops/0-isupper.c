4213b6616a7beea6560cba064d9cc55ae7bb6ede
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-25 06:53:37 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,9 @@
  * @c: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
-
-int _isupper(char x)
+int _isupper(char c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
