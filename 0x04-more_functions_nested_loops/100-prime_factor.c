65bbf01b40b32bba439d8b067c44753c9b03d971
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-26 14:16:22 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- print largest prime factor of 612852475143
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int n, l;
+	long int m = 612852475143;
+
+	for (l = 2; l <= m; l++)
+	{
+	if (m % l == 0)
+	{
+	m = m / l;
+	n = l;
+	}
+	}
+	printf("%d\n", n);
+	return (0);
+}
