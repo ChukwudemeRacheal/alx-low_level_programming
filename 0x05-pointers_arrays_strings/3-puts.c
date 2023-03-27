5dba93d0c5e7ecabf65320fa3603e318ac644649
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-27 11:33:16 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,12 +2,14 @@
 /**
  * _puts - prints a string, followed by a line, to standout
  * @str: string to print
+ * Return: void
  */
+
 void _puts(char *str)
 {
-	while (*str != '\0')
+	for (; *str != '\0'; str++)
 	{
-		_putchar(*str++);
+		_putchar(*str);
 	}
 	-putchar('\n');
 }
