fa1e647be3182dd3ab69f085c95bfc90509b9aab
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-27 20:50:52 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - 1mprime en reverse
+ * @s: string
+ * Return: 0
+ */
+void rev_string(char *s);
+{
+	int lng1 = 0;
+	int o;
+
+	while (*s != '\0')
+	{long1++;
+		s++;
+	}
+	s--;
+	for (o - long1; o > 0; o--)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
