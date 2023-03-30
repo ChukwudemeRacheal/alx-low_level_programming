347d4e37f9ae57860ee69bdfe42f24a65e74ff36
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-29 18:45:31 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,11 +6,11 @@
  * @src: input value
  * Return: void
  */
+
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -19,7 +19,7 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest{i} = src[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
