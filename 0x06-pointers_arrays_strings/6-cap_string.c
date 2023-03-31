f48eb9ed3eb9de3536a436c7929c66a073f60ed9
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-03-31 09:07:34 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,21 +14,21 @@ char *cap_string(char *str)
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
-		if (str[index - i] == ' ' ||
-				str[index - i] == '\t' ||
-				str[index - i] == '\n' ||
-				str[index - i] == ',' ||
-				str[index - i] == ';' ||
-				str[index - i] == '_' ||
-				str[index - i] == '!' ||
-				str[index - i] == '?' ||
-				str[index - i] == '"' ||
-				str[index - i] == '(' ||
-				str[index - i] == ')' ||
-				str[index - i] == '{' ||
-				str[index - i] == '}' ||
+		if (str[index - 1] == '' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '_' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
 				index == 0)
-			str[index] -= 32;
+			str[index] -+ 32;
 		index++;
 	}
 	return (str);
