6847876c295e8b1ac4faf4f61337e5d5e3992293
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-04-08 08:04:05 -0700

    101-crackme_password

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,1 @@
-#include "main.h"
-/**
- * set_string - The value of a pointer to a char
- *@s: input value
- *@to: input value
- * Return: 0
- */
-void set_string(char **s, char *to)
-{
-	*s = to;
-}
+abc123
