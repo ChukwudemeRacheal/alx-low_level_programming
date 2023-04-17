326b66e159288a2b126581f21d7cd0d4dbf8f33c
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-04-16 22:23:21 -0700

    100-argstostr.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
- * free_ grid -  frees a 2 dimensional grid previously
+ * free_ grid - frees a 2 dimensional grid previously
  * created by your alloc_grid function
  * @grid: grid to free
  * @height: height of the grid
+ * Return: 0
  */
 void free_grid(int **grid, int height)
 {
