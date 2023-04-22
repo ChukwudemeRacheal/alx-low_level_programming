c6d3b26ad2dc2616f641640d697097fb59be1298
ChukwudemeRacheal <chukwudeme014@gmail.com>
2023-04-21 22:34:52 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -32,8 +32,10 @@ int main(int argc, char *argv[])
 	{
 		if (i == bytes - 1)
 		{
-			printf("%02hhx", arr[i]);
+			printf("%02hhx\n", arr[i]);
+			break;
 		}
-		return (0);
+		printf("%02hhx", arr[i]):
 	}
+	return (0);
 }
